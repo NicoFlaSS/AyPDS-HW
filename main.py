7e5088a0c7f3e524aa7452229b5e7ad46b80cfa2
NicoFlaSs <flassnico@gmail.com>
2025-05-30 18:42:14 -0600

    main.py added

--- main.py
+++ main.py
@@ -7,13 +7,13 @@ def main(options):
         if len(options) > 2:
             understanding_freq(options[2])
         else:
-            print("Por favor proporciona una frecuencia. Ejemplo: python main.py tarea_2 2")
+            print("Please give a frequency. Example: python main.py act_2 2")
 
 if __name__ == '__main__':
     args = sys.argv
     if len(args) > 1:
         main(args)
     else:
-        print("Por favor proporciona un argumento.")
-        print("Ejemplo (ejecutar tarea 1): python main.py tarea_1")
-        print("Ejemplo (ejecutar tarea 2): python main.py tarea_2 2")
+        print("Please give an argument")
+        print("Example ( run activity 1 ) : python main.py act_1")
+        print("Example ( run activity 2 ) : python main.py act_2 1")
